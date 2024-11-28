d1c6685ba10d214df95a3c70138baca82aa73f69
Royi Aizenstain <Royi.aizen@gmail.com>
2024-11-28 17:06:22 +0200

    Added some comments

--- Advanced Programing-Bash Scripting/wordFinder.sh
+++ Advanced Programing-Bash Scripting/wordFinder.sh
@@ -1,65 +1,88 @@
 #!/bin/bash
+ 
+ #BASH script for assignment 1 
 
+#stores possebility of ending the script.
+#0-keep going, 1-end it
 end=0
 
+#valid min number of arguments
 if(( $#<3 )); then 
 	>&2 echo "Number of parameters received : "$# 
+	echo "Usage : wordFinder.sh <valid file name> [More Files] ... <char> <length>"
 	exit 1
 
 fi
 
+#get the char argument and convert to lowercase
 char=${@: -2:1}
 char="${char,}"
-minWordsTolookFor=${*: -1:1}
 
+#minimum letters To look for in the files
+minlettersTolookFor=${*: -1:1}
 
+#valid char
 if [[ $char != [0-9a-zA-z] ]]; then
 	>&2 echo "Only one char needed : "$char
 	end=1
 fi
 
-if (( $minWordsTolookFor+0 <= 0 )); then
-	>&2 echo "Not a positive number : "$minWordsTolookFor
+#valid number letters To look for in the files
+if (( $minlettersTolookFor+0 <= 0 )); then
+	>&2 echo "Not a positive number : "$minlettersTolookFor
 	end=1
 fi
 
+#if the validation didnt achived then we send the right format and end the script
 if (( end == 1 )); then
 	echo "Usage : wordFinder.sh <valid file name> [More Files] ... <char> <length>"
 	exit 1
 fi
 
-mylist=()
-   
+#List that will store all the files
+myfilelist=()
+
+#loop over all the arguments   
 cnt=0
 for i in $@;
 do
+	#pass the last two args
 	if (( $# - cnt == 2 )); then
     	break 
     fi
+
+    #valid file path
     if [ ! -f "$i" ]; then
    		>&2 echo "File does not exist : "$i
     end=1
 	fi
-	mylist+=($i)
+
+	#add to file list
+	myfilelist+=($i)
+
+	#conter
 	let cnt++
 done
 
+#if the validation of files didnt achived then we send the right format and end the script
 if (( end == 1 )); then
 	echo "Usage : wordFinder.sh <valid file name> [More Files] ... <char> <length>"
 	exit 1
 fi
 
-
-for i in "${!mylist[@]}"
+#combine all the words from each file by the right spartion in long_words 
+for i in "${!myfilelist[@]}"
 do
-	long_words+=$(cat "${mylist[$i]}" | tr -cs 'a-zA-Z0-9' '[\n*]' | grep '^[a-zA-Z0-9]*$') 
+	long_words+=$(cat "${myfilelist[$i]}" | tr -cs 'a-zA-Z0-9' '[\n*]' | grep '^[a-zA-Z0-9]*$') 
 done
 
-awk -v char=$char -v minWordsTolookFor=$minWordsTolookFor '{
+#counting with awk on long_words for each word that start with char and with the minimum number of letters To look for in each word
+#then sort it and then print it with piping 
+awk -v char=$char -v minlettersTolookFor=$minlettersTolookFor '{
 		for(i=1;i<=NF;i++)
 		{ 
 			$i = tolower($i)
-			if(substr($i,0,1)==char && length($i) >= minWordsTolookFor)
+			if(substr($i,0,1)==char && length($i) >= minlettersTolookFor)
 			{
 				a[$i]++
 			}
